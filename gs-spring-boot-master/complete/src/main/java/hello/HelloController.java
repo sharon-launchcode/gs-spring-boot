e1efac382e6412c5045946c855cb2de0e4a3b16c
Phyllis Scheidt <phyllisscheidt@Phyllis-Scheidts-iMac.local>
2018-09-01 20:45:07 -0400

    added iframe dimensions

--- gs-spring-boot-master/complete/src/main/java/hello/HelloController.java
+++ gs-spring-boot-master/complete/src/main/java/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         // return "Greetings from Spring Boot, You Fine Woman You!";
-        return "<iframe src='https://console.dialogflow.com/api-client/demo/embedded/2826f62d-5973-4ab8-b5c8-84a4d026e807'></iframe>";
+        return "<iframe width='350' height='430' src='https://console.dialogflow.com/api-client/demo/embedded/2826f62d-5973-4ab8-b5c8-84a4d026e807'></iframe>";
     }
     
 }
